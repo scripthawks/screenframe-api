fa16faf27664d4e98b2444bf83b8cf4064a17de4
polinaShih <137808704+Polina969@users.noreply.github.com>
2025-11-22 16:03:26 +0300

    QQ-22 feat(signUp.unput-dto.ts) add dto for registararion user

---
+++ apps/main-gateway/src/user-accounts/auth/api/models/input-dto/sign-up.input-dto.ts
@@ -0,0 +1,35 @@
+import { TrimIsString } from '@app/core/decorators/validation';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsEmail, Length, Matches } from 'class-validator';
+import {
+  EMAIL_REGEX,
+  PASS_MAX_LENGTH,
+  PASS_MIN_LENGTH,
+  PASSWORD_REGEX,
+  USERNAME_MAX_LENGTH,
+  USERNAME_MIN_LENGTH,
+  USERNAME_REGEX,
+} from '../../../../core/constants/dto.constants';
+
+export class SignUpUserInputDto {
+  @IsNotEmpty()
+  @Length(USERNAME_MIN_LENGTH, USERNAME_MAX_LENGTH)
+  @Matches(USERNAME_REGEX)
+  @TrimIsString()
+  @ApiProperty()
+  userName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Matches(EMAIL_REGEX)
+  @TrimIsString()
+  @ApiProperty()
+  email: string;
+
+  @IsNotEmpty()
+  @Length(PASS_MIN_LENGTH, PASS_MAX_LENGTH)
+  @Matches(PASSWORD_REGEX)
+  @TrimIsString()
+  @ApiProperty()
+  password: string;
+}
