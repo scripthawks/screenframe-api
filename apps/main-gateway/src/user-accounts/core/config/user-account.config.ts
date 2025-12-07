4787e09da1058b919fb27e32335720658eec1162
polinaShih <137808704+Polina969@users.noreply.github.com>
2025-12-07 19:09:11 +0300

    QQ-33 feat(userAccountConfig) add configs for jwt token

--- apps/main-gateway/src/user-accounts/core/config/user-account.config.ts
+++ apps/main-gateway/src/user-accounts/core/config/user-account.config.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { BaseConfig } from '@app/core/config';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 @Injectable()
 export class UserAccountConfig extends BaseConfig {
@@ -14,11 +14,46 @@ export class UserAccountConfig extends BaseConfig {
   )
   CONFIRMATION_TOKEN_EXPIRATION: number;
 
+  @IsString({
+    message: 'Set Env variable ACCESS_TOKEN_SECRET, GROUP: Dangerous!',
+  })
+  ACCESS_TOKEN_SECRET: string;
+
+  @IsString({
+    message: 'Set Env variable ACCESS_TOKEN_EXPIRATION, GROUP: Dangerous!',
+  })
+  ACCESS_TOKEN_EXPIRATION: string;
+
+  @IsString({
+    message: 'Set Env variable REFRESH_TOKEN_SECRET, GROUP: Dangerous!',
+  })
+  REFRESH_TOKEN_SECRET: string;
+
+  @IsString({
+    message: 'Set Env variable REFRESH_TOKEN_EXPIRATION, GROUP: Dangerous!',
+  })
+  REFRESH_TOKEN_EXPIRATION: string;
+
   constructor(private configService: ConfigService) {
     super();
+
     this.CONFIRMATION_TOKEN_EXPIRATION = Number(
       this.configService.getOrThrow('CONFIRMATION_TOKEN_EXPIRATION'),
     );
+
+    this.ACCESS_TOKEN_SECRET = this.configService.getOrThrow(
+      'ACCESS_TOKEN_SECRET',
+    );
+    this.ACCESS_TOKEN_EXPIRATION = this.configService.getOrThrow(
+      'ACCESS_TOKEN_EXPIRATION',
+    );
+    this.REFRESH_TOKEN_SECRET = this.configService.getOrThrow(
+      'REFRESH_TOKEN_SECRET',
+    );
+    this.REFRESH_TOKEN_EXPIRATION = this.configService.getOrThrow(
+      'REFRESH_TOKEN_EXPIRATION',
+    );
+
     this.validateConfig();
   }
 }
