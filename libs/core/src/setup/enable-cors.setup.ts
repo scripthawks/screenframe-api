7901de0e6fac00c4e3174fa544d1d16ddbe35775
polinaShih <137808704+Polina969@users.noreply.github.com>
2026-03-02 19:47:41 +0300

    excess removed for cross domain

--- libs/core/src/setup/enable-cors.setup.ts
+++ libs/core/src/setup/enable-cors.setup.ts
@@ -11,7 +11,7 @@ export function enableCorsSetup(app: INestApplication, coreConfig: CoreConfig) {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type'],
-    exposedHeaders: ['Authorization', 'Content-Type'],
+    exposedHeaders: ['Authorization'],
     maxAge: 3600,
   });
 }
